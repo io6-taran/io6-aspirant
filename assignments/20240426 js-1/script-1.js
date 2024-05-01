c078dfd75bc0e808bb85576981c7cca7a7a2cb31
taran <io6.taran1@gmail.com>
2024-05-01 19:46:14 +0530

    20240426 js-1

---
+++ assignments/20240426 js-1/script-1.js
@@ -0,0 +1,23 @@
+1.What is the data-type of
+ a. 'Italy'-->String.
+ b. 501-->number
+ c. false-->boolean
+ d. [23,546,67,8,90]-->array
+ e. undefinded-->undefined
+ f. null-->
+ g. {}-->object
+ h.Number(india)-->nan
+ i.Date()-->object
+ j. ?-->
+
+ 2. Find output
+	console.log("output-1", "500" + 100); -->500100
+	console.log("output-2", 56 + 100); -->156
+	console.log("output-3", 56 + "100");  -->56100
+	console.log("output-4", "500" / "100");  -->5
+	console.log("output-5", 56 + 100 + true);  -->56100true
+	console.log("output-6", 56 + 100 + Number(true));  -->56100number
+	console.log("output-7", 56 + 100 + "true");  -->56100true
+	console.log("output-8", 56 + 100 + Number("true"));  -->56100true
+	console.log("output-9", 56 + 100 + Number("Vinay"));  -->56100vinay
+	console.log("output-9", 50 - "100" + "Vinay");  -->-50vinay
