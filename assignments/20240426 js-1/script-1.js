46787c364620a7125b029a58dedabf08e909b328
taran <io6.taran1@gmail.com>
2024-05-01 19:54:14 +0530

    20240426 js-1

--- assignments/20240426 js-1/script-1.js
+++ assignments/20240426 js-1/script-1.js
@@ -4,7 +4,7 @@
  c. false-->boolean
  d. [23,546,67,8,90]-->array
  e. undefinded-->undefined
- f. null-->
+ f. null-->object
  g. {}-->object
  h.Number(india)-->nan
  i.Date()-->object
@@ -15,9 +15,9 @@
 	console.log("output-2", 56 + 100); -->156
 	console.log("output-3", 56 + "100");  -->56100
 	console.log("output-4", "500" / "100");  -->5
-	console.log("output-5", 56 + 100 + true);  -->56100true
-	console.log("output-6", 56 + 100 + Number(true));  -->56100number
-	console.log("output-7", 56 + 100 + "true");  -->56100true
-	console.log("output-8", 56 + 100 + Number("true"));  -->56100true
-	console.log("output-9", 56 + 100 + Number("Vinay"));  -->56100vinay
+	console.log("output-5", 56 + 100 + true);  -->157
+	console.log("output-6", 56 + 100 + Number(true));  -->157
+	console.log("output-7", 56 + 100 + "true");  -->156true
+	console.log("output-8", 56 + 100 + Number("true"));  -->nan
+	console.log("output-9", 56 + 100 + Number("Vinay"));  -->nan
 	console.log("output-9", 50 - "100" + "Vinay");  -->-50vinay
