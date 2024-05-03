8394ad47cb7390a444f42d7ce9934a22b793bb03
taran <io6.taran1@gmail.com>
2024-05-03 19:54:04 +0530

    20240427 js-2

---
+++ assignments/20240427 js-2/script.js
@@ -0,0 +1,17 @@
+// ques:1--> Write a function to print your name, hobbies//
+function myname() {
+  console.log("my name is Taranpreet Kaur");
+}
+function hobbie() {
+  console.log("my hobbie is reading stories");
+}
+//ques-2-->Write a function to enter your you favorite avengers and concat with string
+// Input: entered {avenger-name} from keyboard
+//Output: My favorite avenger is {avenger-name}. I am dying heart fan of {avenger-name}//
+
+// const studentName = prompt("Enter your studentName");
+// console.log("studentName--", studentName);
+
+const avengername = prompt("Enter avengername");
+console.log("My favorite avenger is Thor. I am dying heart fan of Thor");
+console.log("My favorite avenger is ironman. I am dying heart fan of ironman");
