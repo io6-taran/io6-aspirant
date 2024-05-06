7f43bfaa57392d77ceac77bcea35aadfa7c2fe63
taran <io6.taran1@gmail.com>
2024-05-07 01:05:43 +0530

    20240427 js-2

--- assignments/20240427 js-2/script-1.js
+++ assignments/20240427 js-2/script-1.js
@@ -1,4 +1,249 @@
-//print 10 table
+//print table of 1
 for (let i = 1; i <= 100; i++) {
-  console.log(`1 x ${i}=${1 * 1}`);
+  console.log(`1 x ${i}=${1 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`2 x ${i}=${2 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`3 x ${i}=${3 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`4 x ${i}=${4 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`5 x ${i}=${5 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`6 x ${i}=${6 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`7 x ${i}=${7 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`8 x ${i}=${8 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`9 x ${i}=${9 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`10 x ${i}=${10 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`11 x ${i}=${11 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`12 x ${i}=${12 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`13 x ${i}=${13 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`14 x ${i}=${14 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`15 x ${i}=${15 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`16 x ${i}=${16 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`17 x ${i}=${17 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`18 x ${i}=${18 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`19 x ${i}=${19 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`20 x ${i}=${20 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`21 x ${i}=${21 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`22 x ${i}=${22 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`23 x ${i}=${23 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`24 x ${i}=${24 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`25 x ${i}=${25 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`26 x ${i}=${26 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`27 x ${i}=${27 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`28 x ${i}=${28 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`29 x ${i}=${29 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`30 x ${i}=${30 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`31 x ${i}=${31 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`32 x ${i}=${32 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`33 x ${i}=${33 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`34 x ${i}=${34 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`35 x ${i}=${35 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`36 x ${i}=${36 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`37 x ${i}=${37 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`38 x ${i}=${38 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`39 x ${i}=${39 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`40 x ${i}=${40 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`41 x ${i}=${41 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`42 x ${i}=${42 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`43 x ${i}=${43 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`44 x ${i}=${44 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`45 x ${i}=${45 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`46 x ${i}=${46 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`47 x ${i}=${47 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`48 x ${i}=${48 * i}`);
+}
+
+// print table of 2
+for (let i = 1; i <= 100; i++) {
+  console.log(`49 x ${i}=${49 * i}`);
+}
+
+//print table of 1
+for (let i = 1; i <= 100; i++) {
+  console.log(`50 x ${i}=${50 * i}`);
 }
