c927bd878087f2db733cf074086f34db7af293e8
taran <io6.taran1@gmail.com>
2024-05-07 21:14:47 +0530

    20240427 js-2

--- assignments/20240426 js-1/script.js
+++ assignments/20240426 js-1/script.js
@@ -1,5 +1,5 @@
-1.What is the data-type of
- a. 'Italy'-->String.
+//1.What is the data-type of
+/* a. 'Italy'-->String.
  b. 501-->number
  c. false-->boolean
  d. [23,546,67,8,90]-->array
@@ -9,9 +9,9 @@
  h.Number(india)-->nan
  i.Date()-->object
  j. ?-->
-
- 2. Find output
-	console.log("output-1", "500" + 100); -->500100
+*/
+//2. Find output
+/*console.log("output-1", "500" + 100); -->500100
 	console.log("output-2", 56 + 100); -->156
 	console.log("output-3", 56 + "100");  -->56100
 	console.log("output-4", "500" / "100");  -->5
@@ -21,3 +21,10 @@
 	console.log("output-8", 56 + 100 + Number("true"));  -->nan
 	console.log("output-9", 56 + 100 + Number("Vinay"));  -->nan
 	console.log("output-9", 50 - "100" + "Vinay");  -->-50vinay
+*/
+
+for (let i = 20; i <= 30; i++) {
+  for (let j = 20; j <= 10; j++) {
+    console.log(`table of ${i} x ${j}`, i * j);
+  }
+}
--- assignments/20240427 js-2/script-1.js
+++ assignments/20240427 js-2/script-1.js
@@ -247,3 +247,12 @@ for (let i = 1; i <= 100; i++) {
 for (let i = 1; i <= 100; i++) {
   console.log(`50 x ${i}=${50 * i}`);
 }
+
+for (let i = 1; i <= 50; i++) {
+  console.log(i);
+}
+for (let i = 20; i <= 30; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(`table of ${i} x ${j}`, i * j);
+  }
+}
